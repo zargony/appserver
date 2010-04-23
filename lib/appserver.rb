e698fdfc4f6f39de618728c41fd815bbb4710a8e
Andreas Neuhaus <zargony@gmail.com>
2010-04-24 01:48:15 +0200

    Added server class

--- lib/appserver.rb
+++ lib/appserver.rb
@@ -1,6 +1,7 @@
 module Appserver
   ROOT = File.expand_path('..', __FILE__)
   autoload :Command, "#{ROOT}/appserver/command"
+  autoload :Server,  "#{ROOT}/appserver/server"
 end
 
 require "#{Appserver::ROOT}/appserver/core_ext"
