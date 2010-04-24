874a6f327ea5b8c3cb6a7d222e99c09f3402f93d
Andreas Neuhaus <zargony@zargony.com>
2010-04-24 03:40:50 +0200

    Ported app class from old Rakefile

--- lib/appserver.rb
+++ lib/appserver.rb
@@ -1,7 +1,8 @@
 module Appserver
   ROOT = File.expand_path('..', __FILE__)
-  autoload :Command, "#{ROOT}/appserver/command"
-  autoload :Server,  "#{ROOT}/appserver/server"
+  autoload :Command,  "#{ROOT}/appserver/command"
+  autoload :Server,   "#{ROOT}/appserver/server"
+  autoload :App,      "#{ROOT}/appserver/app"
 end
 
 require "#{Appserver::ROOT}/appserver/core_ext"
