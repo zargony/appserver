3f7076c7adf00be6c5817f1247ee5a725590522c
Andreas Neuhaus <zargony@zargony.com>
2010-04-25 02:47:09 +0200

    Removed app settings and refactored class from OpenStruct to Struct

--- lib/appserver/server.rb
+++ lib/appserver/server.rb
@@ -1,9 +1,8 @@
-require 'ostruct'
 require 'etc'
 require 'yaml'
 
 module Appserver
-  class Server < OpenStruct
+  class Server < Struct.new(:repo_dir, :monit_conf, :monit_reload, :nginx_conf, :nginx_reload)
     class AlreadyInitializedError < RuntimeError; end
     class NotInitialized < RuntimeError; end
 
@@ -13,22 +12,24 @@ module Appserver
       :monit_reload => '/usr/sbin/monit reload',
       :nginx_conf => 'nginx.conf',
       :nginx_reload => '/usr/sbin/nginx -s reload',
-      :thin => '/usr/local/bin/thin',
-      :thin_opts => '-e production',
-      :instances => 3,
-      :pids_dir => 'tmp/pids',
-      :sockets_dir => 'tmp/sockets',
-      :server_log => 'log/server.log',
-      :max_cpu_usage => nil,
-      :max_memory_usage => nil,
-      :usage_check_cycles => 5,
-      :http_check_timeout => 30,
-      :domain => `/bin/hostname -f`.chomp.gsub(/^[^.]+\./, ''),
-      :access_log => 'log/access.log',
-      :public_dir => 'public',
     }
 
     attr_reader :dir
+
+    def initialize (options = {})
+      # Get dir setting first, since it's needed for loading the config file
+      @settings = options.symbolize_keys!
+      @dir = @settings[:dir] || Dir.pwd
+      # Load config file if exist
+      if dir_initialized?
+        config = (YAML.load_file(config_file) || {}).symbolize_keys!
+        @settings = config.merge(@settings)
+      end
+      # @settings contains config file options overridden by command line options
+      members.each do |key|
+        self[key] = @settings[key] || DEFAULTS[key]
+      end
+    end
 
     def config_file
       File.join(dir, 'appserver.yml')
@@ -38,38 +39,28 @@ module Appserver
       File.expand_path('../appserver.yml', __FILE__)
     end
 
-    def initialize (options = {})
-      settings = DEFAULTS
-      # Load configuration from given server directory, fall back to the current directory
-      @dir = options.delete(:dir) || Dir.pwd
-      if File.exist?(config_file)
-        config_settings = YAML.load_file(config_file)
-        settings.merge!((config_settings || {}).symbolize_keys!)
-        @dir_initialized = true
-      end
-      # Let command line options override any settings
-      settings.merge!(options)
-      super(settings)
-    end
-
     def dir_initialized?
-      @dir_initialized
+      File.exist?(config_file)
     end
 
     def initialize_dir
-      raise AlreadyInitializedError if File.exist?(config_file)
+      raise AlreadyInitializedError if dir_initialized?
       File.safe_replace(config_file) do |f|
         f.puts IO.read(config_file_template)
       end
     end
 
     def apps
-      @app_names ||= Dir.glob(File.join(dir, '*')).select { |f| File.directory?(f) }.map { |f| File.basename(f) }
-      @apps ||= @app_names.map do |name|
-        settings = super && super[name]
-        App.new(self, name, settings)
+      @apps ||= begin
+        app_dirs ||= Dir.glob(File.join(dir, '*')).select { |f| File.directory?(f) }.map { |f| File.basename(f) }
+        app_dirs.map do |name|
+          App.new(self, name, @settings)
+        end
       end
-      @apps
+    end
+
+    def app (name)
+      (@apps || []).find { |app| app.name == name } || App.new(self, name, @settings)
     end
 
     def write_configs
