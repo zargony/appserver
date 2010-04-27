c50f21edf2e547185f96e9af5c8d386594811a33
Andreas Neuhaus <zargony@zargony.com>
2010-04-27 20:32:04 +0200

    Repository#install_hook installs a post-receive hook to the repository that calls the deploy command

--- lib/appserver/repository.rb
+++ lib/appserver/repository.rb
@@ -7,5 +7,35 @@ module Appserver
     def initialize (server, path)
       @server, @path = server, path.chomp('/')
     end
+
+    def name
+      File.basename(path, '.git')
+    end
+
+    def valid?
+      File.directory?(File.join(path, 'hooks')) && File.directory?(File.join(path, 'refs'))
+    end
+
+    def post_receive_hook
+      File.join(path, 'hooks', 'post-receive')
+    end
+
+    def install_hook
+      raise InvalidRepositoryError unless valid?
+      deploy_cmd = "#{File.expand_path($0)} -d #{File.expand_path(server.dir)} deploy #{File.expand_path(path)}"
+      if !File.exist?(post_receive_hook) || !File.executable?(post_receive_hook)
+        puts "Installing git post-receive hook to repository #{path}..."
+        File.safe_replace(post_receive_hook) do |f|
+          f.puts '#!/bin/sh'
+          f.puts deploy_cmd
+          f.chown File.stat(path).uid, File.stat(path).gid
+          f.chmod 0755
+        end
+      elsif !File.readlines(post_receive_hook).any? { |line| line =~ /^#{Regexp.escape(deploy_cmd)}/ }
+        puts "Couldn't install post-receive hook. Foreign hook script already present in repository #{path}!"
+      else
+        #puts "Hook already installed in repository #{path}"
+      end
+    end
   end
 end
