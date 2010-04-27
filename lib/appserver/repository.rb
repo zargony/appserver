9c7a1a0da9d26c6b64323fcb0d528078b4abbc4a
Andreas Neuhaus <zargony@gmail.com>
2010-04-28 01:06:39 +0200

    Use Repository#expand_path to expand a path based on the repository directory

--- lib/appserver/repository.rb
+++ lib/appserver/repository.rb
@@ -1,41 +1,48 @@
 module Appserver
-  class Repository < Struct.new(:server, :path)
+  class Repository < Struct.new(:server, :dir)
     class InvalidRepositoryError < RuntimeError; end
 
     include Utils
 
-    def initialize (server, path, config)
-      self.server, self.path = server, path.chomp('/')
+    def initialize (server, dir, config)
+      self.server, self.dir = server, dir.chomp('/')
       raise InvalidRepositoryError unless valid?
     end
 
     def name
-      File.basename(path, '.git')
+      File.basename(dir, '.git')
     end
 
     def valid?
-      File.directory?(File.join(path, 'hooks')) && File.directory?(File.join(path, 'refs'))
+      File.directory?(File.join(dir, 'hooks')) && File.directory?(File.join(dir, 'refs'))
     end
 
     def post_receive_hook
-      File.join(path, 'hooks', 'post-receive')
+      File.join(dir, 'hooks', 'post-receive')
     end
 
     def install_hook
-      deploy_cmd = "#{File.expand_path($0)} -d #{File.expand_path(server.dir)} deploy #{File.expand_path(path)}"
+      deploy_cmd = "#{File.expand_path($0)} -d #{server.dir} deploy #{dir}"
+      puts deploy_cmd
       if !File.exist?(post_receive_hook) || !File.executable?(post_receive_hook)
-        puts "Installing git post-receive hook to repository #{path}..."
+        puts "Installing git post-receive hook to repository #{dir}..."
         safe_replace_file(post_receive_hook) do |f|
           f.puts '#!/bin/sh'
           f.puts deploy_cmd
-          f.chown File.stat(path).uid, File.stat(path).gid
+          f.chown File.stat(dir).uid, File.stat(dir).gid
           f.chmod 0755
         end
       elsif !File.readlines(post_receive_hook).any? { |line| line =~ /^#{Regexp.escape(deploy_cmd)}/ }
-        puts "Couldn't install post-receive hook. Foreign hook script already present in repository #{path}!"
+        puts "Couldn't install post-receive hook. Foreign hook script already present in repository #{dir}!"
       else
-        #puts "Hook already installed in repository #{path}"
+        #puts "Hook already installed in repository #{dir}"
       end
+    end
+
+  protected
+
+    def expand_path (path)
+      File.expand_path(path, dir)
     end
   end
 end
