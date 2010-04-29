eed6f987a0c592893ec24ed412afdfec3b7d5439
Andreas Neuhaus <zargony@gmail.com>
2010-04-30 01:23:04 +0200

    Implemented deploy method to check out an application from a repository into an application directory

--- lib/appserver/repository.rb
+++ lib/appserver/repository.rb
@@ -1,3 +1,4 @@
+require 'fileutils'
 require 'git'
 
 module Appserver
@@ -42,6 +43,32 @@ module Appserver
         puts "Couldn't install post-receive hook. Foreign hook script already present in repository #{dir}!"
       else
         #puts "Hook already installed in repository #{dir}"
+      end
+    end
+
+    def deploy
+      # Choose a temporary build directory on the same filesystem so that it
+      # can be easily renamed/moved to be the real application directory later
+      build_dir, old_dir = "#{app.dir}.new", "#{app.dir}.old"
+      begin
+        # Check out the current code
+        checkout(build_dir)
+
+        # TODO: more deploy setup (build gem bundle, write database config, ...)
+
+        # Replace the current application directory with the newly built one
+        FileUtils.rm_rf old_dir
+        FileUtils.mv app.dir, old_dir if Dir.exist?(app.dir)
+        FileUtils.mv build_dir, app.dir
+
+        # TODO: restart instances (needs root, use monit?)
+        # TODO: remove old_dir *after* restart succeeded, maybe revert to old_dir on failure
+
+      ensure
+        # If anything broke and the build directory still exists, remove it
+        FileUtils.rm_rf build_dir
+        # If anything broke and the app directory doesn't exist anymore, put the old directory in place
+        FileUtils.mv old_dir, app.dir if !Dir.exist?(app.dir) && Dir.exist?(old_dir)
       end
     end
 
