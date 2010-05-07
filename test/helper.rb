4235ee4e99822bcf51f7cdf40d482a4fe66049a0
Andreas Neuhaus <zargony@gmail.com>
2010-05-07 19:57:21 +0200

    Added unit testing stuff

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'mocha'
+
+$LOAD_PATH.unshift(File.expand_path('../../lib', __FILE__))
+$LOAD_PATH.unshift(File.expand_path('..', __FILE__))
+require 'appserver'
+
+class Test::Unit::TestCase
+end
