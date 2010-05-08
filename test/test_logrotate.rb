12c35aeffa5e34f08e1ba9c6b47cf2fec5283b28
Andreas Neuhaus <zargony@gmail.com>
2010-05-08 16:11:22 +0200

    Generate minimal number of logrotate statements to minimize reopen calls

--- test/test_logrotate.rb
+++ test/test_logrotate.rb
@@ -6,6 +6,7 @@ class TestLogrotate < Test::Unit::TestCase
   def test_write_config
     in_server_dir do |server_dir|
       create_dummy_rack_app('apps/hello')
+      create_dummy_rack_app('apps/hello2')
       assert !File.exist?('logrotate.conf')
       Appserver::Logrotate.write_config(server_dir)
       assert File.exist?('logrotate.conf')
