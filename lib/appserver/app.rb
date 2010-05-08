c48e1f06d0056a957313ce89e3accbe67c59f672
Andreas Neuhaus <zargony@zargony.com>
2010-05-08 15:06:42 +0200

    Minor refactoring

--- lib/appserver/app.rb
+++ lib/appserver/app.rb
@@ -26,39 +26,41 @@ module Appserver
       :public_dir => 'public',
     }
 
-    def self.unicorn_config
-      File.expand_path('../unicorn.conf.rb', __FILE__)
-    end
+    ALWAYS_WHITELIST = ['PATH', 'PWD', 'GEM_HOME', 'GEM_PATH', 'RACK_ENV']
 
     def initialize (server, name, config)
-      super()
       self.server, self.name = server, name
+      # Application-specific configuration settings
       appconfig = (config[:apps] || {})[name.to_sym] || {}
       DEFAULTS.each do |key, default_value|
         self[key] = appconfig[key] || config[key] || default_value
       end
       # Use the directory owner as the user to run instances under by default
-      self.user ||= exist? ? Etc.getpwuid(File.stat(dir).uid).name : 'www-data'
+      self.user ||= exist? ? Etc.getpwuid(File.stat(path).uid).name : 'www-data'
       # Make array from comma separated list
       self.env_whitelist = env_whitelist.split(/\s*,\s*/) if String === env_whitelist
       # Use a subdomain of the default hostname if no hostname was given specifically for this app
       self.hostname = "#{name.gsub(/[^a-z0-9_-]+/i, '_')}.#{hostname}" unless appconfig[:hostname]
     end
 
-    def dir
+    def path
       File.join(server.apps_path, name)
     end
 
     def exist?
-      File.exist?(dir)
+      File.directory?(path)
     end
 
     def revision_file
-      File.join(dir, 'REVISION')
+      File.join(path, 'REVISION')
+    end
+
+    def public_path
+      File.expand_path(public_dir, path)
     end
 
     def rack_config
-      File.join(dir, 'config.ru')
+      File.join(path, 'config.ru')
     end
 
     def rack?
@@ -69,27 +71,46 @@ module Appserver
       rack?
     end
 
-    def gem_file
-      File.join(dir, 'Gemfile')
+    def unicorn_config
+      File.expand_path('../unicorn.conf.rb', __FILE__)
     end
 
-    def bundle_dir
-      File.join(dir, '.bundle')
+    def gem_file
+      File.join(path, 'Gemfile')
+    end
+
+    def bundle_path
+      File.join(path, '.bundle')
+    end
+
+    def pid_file
+      File.join(server.tmp_path, "#{name}.pid")
+    end
+
+    def socket
+      File.join(server.tmp_path, "#{name}.socket")
+    end
+
+    def server_log
+      File.join(server.log_path, "#{name}.server.log")
+    end
+
+    def access_log
+      File.join(server.log_path, "#{name}.access.log")
     end
 
     def setup_env!
       # Apply whitelist if set
       if env_whitelist != ['*']
-        always_whitelist = ['PATH', 'PWD', 'GEM_HOME', 'GEM_PATH', 'RACK_ENV']
-        ENV.reject! { |key, value| !env_whitelist.include?(key) && !always_whitelist.include?(key) }
+        ENV.reject! { |key, value| !env_whitelist.include?(key) && !ALWAYS_WHITELIST.include?(key) }
       end
       # Set environment variables
       if env
         ENV.update(env)
       end
       # Setup gem bundle if present
-      if File.exist?(gem_file) && File.directory?(bundle_dir)
-        ENV.update({ 'GEM_HOME' => bundle_dir, 'BUNDLE_PATH' => bundle_dir })
+      if File.exist?(gem_file) && File.directory?(bundle_path)
+        ENV.update({ 'GEM_HOME' => bundle_path, 'BUNDLE_PATH' => bundle_path })
         # Remember load paths of required gems (which use autloading), before bundler takes away the load path
         remember_paths = $LOAD_PATH.select { |path| path =~ %r(/(unicorn|rack|appserver)[^/]*/) }
         # Load bundler and setup gem bundle
@@ -100,37 +121,21 @@ module Appserver
       end
     end
 
-    def pid_file
-      File.join(server.tmp_dir, "#{name}.pid")
-    end
-
-    def socket
-      File.join(server.tmp_dir, "#{name}.socket")
-    end
-
-    def server_log
-      File.join(server.log_dir, "#{name}.server.log")
-    end
-
-    def access_log
-      File.join(server.log_dir, "#{name}.access.log")
-    end
-
     def write_monit_config (f)
       f.puts %Q()
       f.puts %Q(# Application: #{name})
       if rack?
         cyclecheck = usage_check_cycles > 1 ? " for #{usage_check_cycles} cycles" : ''
-        f.puts %Q(check process #{name} with pidfile #{expand_path(pid_file)})
-        f.puts %Q(  start program = "#{ruby} -S -- unicorn -E #{environment} -Dc #{self.class.unicorn_config} #{rack_config}")
-        f.puts %Q(  stop program = "/bin/bash -c 'kill -TERM `cat #{expand_path(pid_file)}`'")
+        f.puts %Q(check process #{name} with pidfile #{pid_file})
+        f.puts %Q(  start program = "#{ruby} -S -- unicorn -E #{environment} -Dc #{unicorn_config} #{rack_config}")
+        f.puts %Q(  stop program = "/bin/bash -c 'kill -TERM `cat #{pid_file}`'")
         f.puts %Q(  if totalcpu usage > #{max_cpu_usage}#{cyclecheck} then restart) if max_cpu_usage
         f.puts %Q(  if totalmemory usage > #{max_memory_usage}#{cyclecheck} then restart) if max_memory_usage
-        f.puts %Q(  if failed unixsocket #{expand_path(socket)} protocol http request "/" timeout #{http_check_timeout} seconds then restart) if http_check_timeout > 0
+        f.puts %Q(  if failed unixsocket #{socket} protocol http request "/" timeout #{http_check_timeout} seconds then restart) if http_check_timeout > 0
         f.puts %Q(  if 5 restarts within 5 cycles then timeout)
         f.puts %Q(  group appserver)
-        f.puts %Q(check file #{name}_revision with path #{expand_path(revision_file)})
-        f.puts %Q(  if changed checksum then exec "/bin/bash -c 'kill -USR2 `cat #{expand_path(pid_file)}`'")
+        f.puts %Q(check file #{name}_revision with path #{revision_file})
+        f.puts %Q(  if changed checksum then exec "/bin/bash -c 'kill -USR2 `cat #{pid_file}`'")
       end
     end
 
@@ -139,13 +144,13 @@ module Appserver
       f.puts "# Application: #{name}"
       if rack?
         f.puts "upstream #{name} {"
-        f.puts "  server unix:#{expand_path(socket)} fail_timeout=0;"
+        f.puts "  server unix:#{socket} fail_timeout=0;"
         f.puts "}"
         f.puts "server {"
         f.puts "  listen 80;"
         f.puts "  server_name #{hostname};"
-        f.puts "  root #{expand_path(public_dir)};"
-        f.puts "  access_log #{expand_path(access_log)};"
+        f.puts "  root #{public_path};"
+        f.puts "  access_log #{access_log};"
         # TODO: maintenance mode rewriting
         f.puts "  try_files $uri/index.html $uri.html $uri @#{name};"
         f.puts "  location @#{name} {"
@@ -168,8 +173,8 @@ module Appserver
           f.puts "  ssl_protocols SSLv2 SSLv3 TLSv1;"
           f.puts "  ssl_ciphers ALL:!ADH:!EXPORT56:RC4+RSA:+HIGH:+MEDIUM:+LOW:+SSLv2:+EXP;"
           f.puts "  ssl_prefer_server_ciphers on;"
-          f.puts "  root #{expand_path(public_dir)};"
-          f.puts "  access_log #{expand_path(access_log)};"
+          f.puts "  root #{public_path};"
+          f.puts "  access_log #{access_log};"
           # TODO: maintenance mode rewriting
           f.puts "  try_files $uri/index.html $uri.html $uri @#{name};"
           f.puts "  location @#{name} {"
@@ -190,21 +195,15 @@ module Appserver
       f.puts ""
       f.puts "# Application: #{name}"
       if rack?
-        f.puts "#{expand_path(server_log)} {"
+        f.puts "#{server_log} {"
         f.puts "  missingok"
         f.puts "  delaycompress"
         f.puts "  sharedscripts"
         f.puts "  postrotate"
-        f.puts "    kill -USR1 `cat #{expand_path(pid_file)}`"
+        f.puts "    kill -USR1 `cat #{pid_file}`"
         f.puts "  endscript"
         f.puts "}"
       end
-    end
-
-  protected
-
-    def expand_path (path)
-      File.expand_path(path, dir)
     end
   end
 end
