0d31e614717aeff0a50150f92fff0bfaf62c878b
Andreas Neuhaus <zargony@gmail.com>
2010-05-11 23:28:13 +0200

    Silence const redefine warning in Ruby 1.8

--- lib/appserver.rb
+++ lib/appserver.rb
@@ -1,5 +1,5 @@
 module Appserver
-  ROOT = File.expand_path('..', __FILE__)
+  ROOT = File.expand_path('..', __FILE__) unless const_defined?(:ROOT)
   autoload :Utils,        "#{ROOT}/appserver/utils"
   autoload :Command,      "#{ROOT}/appserver/command"
   autoload :Configurator, "#{ROOT}/appserver/configurator"
